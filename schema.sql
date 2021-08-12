0157881d54e15ba853bc7cd8c774af5cd4bbfc5a
tim24ktk <tim24ktk@mail.ru>
2021-08-12 22:38:40 +0300

    Правки замечаний

--- schema.sql
+++ schema.sql
@@ -5,64 +5,89 @@ CREATE DATABASE readme
 USE readme;
 
 CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   registration_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  email VARCHAR(255) UNIQUE,
-  user_login VARCHAR(255) UNIQUE,
-  user_password VARCHAR(255),
+  email VARCHAR(255) NOT NULL UNIQUE,
+  login VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255),
   avatar VARCHAR(255)
 );
 
-CREATE TABLE posts (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  date_creation DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  post_title VARCHAR(255) NOT NULL,
-  content VARCHAR(255),
-  quote_author VARCHAR(255),
-  post_image VARCHAR(255),
-  post_video VARCHAR(255) ,
-  website_Link VARCHAR(255),
-  views_number INT DEFAULT 0,
-  author_id INT,
-  content_type_id INT,
-  hashtag_id INT
-);
-
-CREATE TABLE comments (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  comment_content VARCHAR(255) NOT NULL,
-  author_id INT,
-  post_id INT
-);
-
-CREATE TABLE likes (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  user_id INT,
-  liked_post INT
-);
-
-CREATE TABLE subscription (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  follower INT,
-  user_id INT
-);
-
-CREATE TABLE messages (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  message_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  message_content VARCHAR(255),
-  sender INT,
-  recipient INT
+CREATE TABLE content_type (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  content_title VARCHAR(255) NOT NULL,
+  icon_class VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE hashtags (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   hashtag VARCHAR(255)
 );
 
-CREATE TABLE content_type (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  content_title VARCHAR(255) NOT NULL,
-  icon_class VARCHAR(255) NOT NULL
+CREATE TABLE posts (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  date_creation DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title VARCHAR(255) NOT NULL,
+  content VARCHAR(255),
+  quote_author VARCHAR(255),
+  image VARCHAR(255),
+  video VARCHAR(255),
+  website_link VARCHAR(255),
+  views_number INT DEFAULT 0,
+  author_id INT UNSIGNED,
+  type_id INT UNSIGNED,
+  hashtag_id INT UNSIGNED,
+
+  FOREIGN KEY (author_id) REFERENCES users (id),
+  FOREIGN KEY (type_id) REFERENCES content_type (id),
+  FOREIGN KEY (hashtag_id) REFERENCES hashtags (id)
+);
+
+CREATE TABLE comments (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  content VARCHAR(255) NOT NULL,
+  author_id INT UNSIGNED,
+  post_id INT UNSIGNED,
+
+  FOREIGN KEY (author_id) REFERENCES users (id),
+  FOREIGN KEY (post_id) REFERENCES posts (id)
+);
+
+CREATE TABLE subscription (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  follower INT UNSIGNED,
+  user_id INT UNSIGNED,
+
+  FOREIGN KEY (follower) REFERENCES users (id),
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE likes (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  user_id INT UNSIGNED,
+  liked_post INT UNSIGNED,
+
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (liked_post) REFERENCES posts (id)
+);
+
+
+CREATE TABLE messages (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  message_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  content VARCHAR(255),
+  sender INT UNSIGNED ,
+  recipient INT UNSIGNED,
+
+  FOREIGN KEY (sender) REFERENCES users (id),
+  FOREIGN KEY (recipient) REFERENCES users (id)
+);
+
+CREATE TABLE posts_hashtags (
+  post_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  hashtag_id INT UNSIGNED,
+
+  FOREIGN KEY (post_id) REFERENCES posts (id),
+  FOREIGN KEY (hashtag_id) REFERENCES hashtags (id)
 );
