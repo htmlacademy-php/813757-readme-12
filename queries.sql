b55f1f014f99300b7c78dc3bb0ab0d72a310a12f
tim24ktk <tim24ktk@mail.ru>
2021-08-15 04:23:22 +0300

    Написал SQL запросы

---
+++ queries.sql
@@ -0,0 +1,53 @@
+/*добавляет список типов контента для поста*/
+INSERT INTO content_type (content_title, icon_class)
+VALUES
+('Цитата', 'post-quote'),
+('Текст', 'post-text'),
+('Фото', 'post-photo'),
+('Ссылка', 'post-link');
+
+/*добавляет выдуманных пользователей*/
+INSERT INTO users (registration_date, email, login, password, avatar)
+VALUES
+('2021.01.02 20:17:21', 'danila@mail.ru', 'Лариса', 'dan09', 'userpic-larisa-small.jpg'),
+('2021.06.22 05:05:05', 'liza@yandex.ru', 'Владик', 'liza06', 'userpic.jpg'),
+('2021.08.13 17:05:43', 'bety@yandex.ru', 'Виктор', 'bety24', 'userpic-mark.jpg');
+
+/*добавляет существующий список постов*/
+INSERT INTO posts (title, type_id, content, views_number, author_id)
+VALUES
+('Цитата', 1, 'Мы в жизни любим только раз, а после ищем лишь похожих', 200, 1),
+('Игра престолов', 2, 'Не могу дождаться начала финального сезона своего любимого сериала!', 3000, 2),
+('Наконец, обработал фотки!', 3, 'rock-medium.jpg', 150, 3),
+('Моя мечта', 3, 'coast-medium.jpg', 10000, 1),
+('Лучшие курсы', 2, 'www.htmlacademy.ru', 400, 2);
+
+/*комментарии к разным постам*/
+INSERT INTO comments (creation_date, content, author_id, post_id)
+VALUES
+('2021.02.02 22:17:21', 'Прекрасная обработка! Так держать!', 1, 3),
+('2021.05.02 12:00:00', 'Просто берет за душу!', 2, 1),
+('2021.08.08 17:02:23', 'Лучше курсов не стречал!', 3, 5);
+
+/*получает список постов с сортировкой по популярности и вместе с именами авторов и типом контента;*/
+SELECT p.title, p.type_id, p.content, p.author_id, p.views_number
+FROM posts AS p, users AS u, content_type AS ct
+WHERE p.author_id = u.id AND p.type_id = ct.id ORDER BY p.views_number DESC;
+
+/*получает список постов для конкретного пользователя*/
+SELECT p.title, p.type_id, p.content, p.author_id, p.views_number
+FROM posts AS p
+LEFT JOIN users AS u ON p.author_id=u.id
+WHERE u.id=1;
+
+/*получает список комментариев для одного поста, в комментариях должен быть логин пользователя*/
+SELECT c.*, u.login
+FROM comments AS c
+LEFT JOIN users AS u ON c.id=u.id
+WHERE c.post_id=1;
+
+/*добавляет лайк посту*/
+INSERT INTO likes (user_id, liked_post) VALUES (2, 4);
+
+/*подписаться на пользователя*/
+INSERT INTO subscription (follower, user_id) VALUES (3, 1);
--- schema.sql
+++ schema.sql
@@ -27,16 +27,16 @@ CREATE TABLE hashtags (
 CREATE TABLE posts (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   date_creation DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  title VARCHAR(255) DEFAULT NOT NULL,
-  content VARCHAR(255) DEFAULT NOT NULL,
-  quote_author VARCHAR(255) DEFAULT NOT NULL,
-  image VARCHAR(255) DEFAULT NOT NULL,
-  video VARCHAR(255) DEFAULT NOT NULL,
-  website_link VARCHAR(255) DEFAULT NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  content VARCHAR(255) NOT NULL,
+  quote_author VARCHAR(255) DEFAULT NULL,
+  image VARCHAR(255) DEFAULT NULL,
+  video VARCHAR(255) DEFAULT NULL,
+  website_link VARCHAR(255) DEFAULT NULL,
   views_number INT DEFAULT 0,
-  author_id INT UNSIGNED DEFAULT NOT NULL,
-  type_id INT UNSIGNED DEFAULT NOT NULL,
-  hashtag_id INT UNSIGNED DEFAULT NOT NULL,
+  author_id INT UNSIGNED,
+  type_id INT UNSIGNED,
+  hashtag_id INT UNSIGNED,
 
   FOREIGN KEY (author_id) REFERENCES users (id),
   FOREIGN KEY (type_id) REFERENCES content_type (id),
@@ -91,3 +91,57 @@ CREATE TABLE posts_hashtags (
   FOREIGN KEY (post_id) REFERENCES posts (id),
   FOREIGN KEY (hashtag_id) REFERENCES hashtags (id)
 );
+
+/*добавляет список типов контента для поста*/
+INSERT INTO content_type (content_title, icon_class)
+VALUES
+('Цитата', 'post-quote'),
+('Текст', 'post-text'),
+('Фото', 'post-photo'),
+('Ссылка', 'post-link');
+
+/*добавляет выдуманных пользователей*/
+INSERT INTO users (registration_date, email, login, password, avatar)
+VALUES
+('2021.01.02 20:17:21', 'danila@mail.ru', 'Лариса', 'dan09', 'userpic-larisa-small.jpg'),
+('2021.06.22 05:05:05', 'liza@yandex.ru', 'Владик', 'liza06', 'userpic.jpg'),
+('2021.08.13 17:05:43', 'bety@yandex.ru', 'Виктор', 'bety24', 'userpic-mark.jpg');
+
+/*комментарии к разным постам*/
+/*INSERT INTO comments (creation_date, content, author_id, post_id)
+VALUES
+('2021.02.02 22:17:21', 'Прекрасная обработка! Так держать!', 1, 3),
+('2021.05.02 12:00:00', 'Просто берет за душу!', 2, 1),
+('2021.08.08 17:02:23', 'Лучше курсов не стречал!', 3, 5);*/
+
+/*добавляет существующий список постов*/
+INSERT INTO posts (title, type_id, content, views_number, author_id)
+VALUES
+('Цитата', 1, 'Мы в жизни любим только раз, а после ищем лишь похожих', 200, 1),
+('Игра престолов', 2, 'Не могу дождаться начала финального сезона своего любимого сериала!', 3000, 2),
+('Наконец, обработал фотки!', 3, 'rock-medium.jpg', 150, 3),
+('Моя мечта', 3, 'coast-medium.jpg', 10000, 1),
+('Лучшие курсы', 2, 'www.htmlacademy.ru', 400, 2);
+
+/*получает список постов с сортировкой по популярности и вместе с именами авторов и типом контента;*/
+SELECT p.title, p.type_id, p.content, p.author_id, p.views_number
+FROM posts AS p, users AS u, content_type AS ct
+WHERE p.author_id = u.id AND p.type_id = ct.id ORDER BY p.views_number DESC;
+
+/*получает список постов для конкретного пользователя*/
+SELECT p.title, p.type_id, p.content, p.author_id, p.views_number
+FROM posts AS p
+LEFT JOIN users AS u ON p.author_id=u.id
+WHERE u.id=1;
+
+/*получает список комментариев для одного поста, в комментариях должен быть логин пользователя*/
+SELECT c.*, u.login
+FROM comments AS c
+LEFT JOIN users AS u ON c.id=u.id
+WHERE c.post_id=1;
+
+/*добавляет лайк посту*/
+INSERT INTO likes (user_id, liked_post) VALUES (2, 4);
+
+/*подписаться на пользователя*/
+INSERT INTO subscription (follower, user_id) VALUES (3, 1);
