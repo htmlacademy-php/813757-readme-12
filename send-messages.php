5ff69c6de0dfc537bfc73669d068d0fc2c9df0fa
tim24ktk <tim24ktk@mail.ru>
2021-11-07 20:25:16 +0300

    установил composer, SwiftMailer и настроил подключение к почтовому серверу

---
+++ send-messages.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$transport = new Swift_SmtpTransport('mailtrap.io', 25);
+$transport->setUsername('keks@phpdemo.ru');
+$transport->setPassword('htmlacademy');
+
+$mailer = new Swift_Mailer($transport);
