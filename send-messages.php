cba6c96d172181ceae8619036e2bbbdb6471c0f6
tim24ktk <tim24ktk@mail.ru>
2021-11-07 22:54:55 +0300

    Добавил скрипт уведомленя о новом подписчике

--- send-messages.php
+++ send-messages.php
@@ -2,8 +2,8 @@
 
 require_once 'vendor/autoload.php';
 
-$transport = new Swift_SmtpTransport('mailtrap.io', 25);
-$transport->setUsername('keks@phpdemo.ru');
-$transport->setPassword('htmlacademy');
+$transport = new Swift_SmtpTransport('smtp.mailtrap.io', 25);
+$transport->setUsername('c2d0a1632ae0c0');
+$transport->setPassword('02f8d42cde5b1a');
 
 $mailer = new Swift_Mailer($transport);
